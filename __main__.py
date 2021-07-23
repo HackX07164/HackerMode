68f9d57493808502873228713ce7f42448713eed
Mohamed - kainai <56244233+No-Name-404@users.noreply.github.com>
2021-07-23 07:05:35 +0200

    fix  path

--- __main__.py
+++ __main__.py
@@ -1,2 +1,17 @@
-import os
+import sys
 
+
+class HackerMode:
+    def install(self):
+        pass
+
+
+if __name__ == "__main__":
+    HackerMode = HackerMode()
+    if len(sys.argv) > 1:
+        try:
+            HackerMode.__getattribute__(sys.argv[1])()
+        except Exception as e:
+            print(e)
+    else:
+        print("help msg")

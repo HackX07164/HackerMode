a0a606bcb76faaa50f1f49817828a60467d9f3dd
G705 <78649813+G705-Ghilan@users.noreply.github.com>
2021-08-30 16:06:50 +0300

    update pyprivate

--- base/bin/pyprivate.py
+++ base/bin/pyprivate.py
@@ -5,184 +5,179 @@ import zlib
 import base64
 import marshal
 import py_compile
+import multiprocessing
 
-from rich.console import Console
+
 from rich.syntax import Syntax
+from rich.filesize import decimal
+from rich.console import Console
+from rich.progress import Progress, BarColumn, TextColumn, SpinnerColumn
 
 
-COPYRIGHT: str = """# Encoded by HackerMode tool...
+copyright: str = b"""# Encoded by HackerMode tool...
 # Copyright: PSH-TEAM
-# Follow us on telegram ( @psh_team )"""
+# Follow us on telegram ( @psh_team )
+"""
 
-RUNPY: str = f"""#!/usr/bin/python3\n{COPYRIGHT}"""
-
-DECODE_SYNTAX = lambda _bytes: RUNPY + '\n' + base64.b64decode(_bytes).decode()
-
-SYNTAX_ENCRYPTION: dict = {
-    "bz2": b'aW1wb3J0IGJ1aWx0aW5zCnggPSBsYW1iZGEgdCwgbTogYnVpbHRpbnMuZXZhbChmIl9faW1wb3J0X18oJ3t7bX19Jyl7e3R9fXByZXNzIikoe3NvdXJjZX0pCmJ1aWx0aW5zLmV4ZWMoYnVpbHRpbnMuY29tcGlsZSh4KCIuZGVjb20iLCAiYnoyIiksInN0cmluZyIsICJleGVjIikp',
-    "zlib": b'aW1wb3J0IGJ1aWx0aW5zCnggPSBsYW1iZGEgdCwgbTogYnVpbHRpbnMuZXZhbChmIl9faW1wb3J0X18oJ3t7bX19Jyl7e3R9fXByZXNzIikoe3NvdXJjZX0pCmJ1aWx0aW5zLmV4ZWMoYnVpbHRpbnMuY29tcGlsZSh4KCIuZGVjb20iLCAiemxpYiIpLCJzdHJpbmciLCAiZXhlYyIpKQ==',
-    "marshal": b'aW1wb3J0IGJ1aWx0aW5zCmltcG9ydCBtYXJzaGFsIGFzIG0KYnVpbHRpbnMuZXhlYyhtLmxvYWRzKHtzb3VyY2V9KSk=',
-    "eval": b'aW1wb3J0IG1hdGgKaW1wb3J0IGJ1aWx0aW5zCmRhdGEgPSAne3NvdXJjZX0nCmkgPSBidWlsdGlucy5ldmFsKGInXHg1Ylx4NmNceDYxXHg2ZFx4NjJceDY0XHg2MVx4MjBceDczXHgzYVx4MjBceDYyXHg3NVx4NjlceDZjXHg3NFx4NjlceDZlXHg3M1x4MmVceDYzXHg2Zlx4NmRceDcwXHg2OVx4NmNceDY1XHgyOFx4NzNceDJjXHgyMFx4MjdceDNjXHg3M1x4NzRceDcyXHg2OVx4NmVceDY3XHgzZVx4MjdceDJjXHgyMFx4MjdceDY1XHg3OFx4NjVceDYzXHgyN1x4MjlceDJjXHgyMFx4NjJceDc1XHg2OVx4NmNceDc0XHg2OVx4NmVceDczXHgyZVx4NjVceDc4XHg2NVx4NjNceDJjXHgyMFx4MjdceDI3XHgyZVx4NmFceDZmXHg2OVx4NmVceDJjXHgyMFx4NmRceDYxXHg3MFx4MmNceDIwXHg2Y1x4NjFceDZkXHg2Mlx4NjRceDYxXHgyMFx4NzRceDNhXHgyMFx4NjNceDY4XHg3Mlx4MjhceDY5XHg2ZVx4NzRceDI4XHg2ZFx4NjFceDc0XHg2OFx4MmVceDczXHg3MVx4NzJceDc0XHgyOFx4NmZceDcyXHg2NFx4MjhceDc0XHgyOVx4MjlceDI5XHgyOVx4NWQnKQppWzFdKGlbLTVdKGlbLTNdKGlbLTJdKGlbLTFdLCBkYXRhKSkpKQ==',
-    "eval2": b'aW1wb3J0IGJ1aWx0aW5zCmJ1aWx0aW5zLmV4ZWMoYnVpbHRpbnMuZXZhbCgne3NvdXJjZX0nKSk=',
-    "binary": b'aW1wb3J0IGJ1aWx0aW5zCmRhdGEgPSB7c291cmNlfQppID0gYnVpbHRpbnMuZXZhbChiJ1x4NWJceDYyXHg3NVx4NjlceDZjXHg3NFx4NjlceDZlXHg3M1x4MmVceDY1XHg3OFx4NjVceDYzXHgyY1x4MjBceDZjXHg2MVx4NmRceDYyXHg2NFx4NjFceDIwXHg3M1x4M2FceDIwXHg2Mlx4NzVceDY5XHg2Y1x4NzRceDY5XHg2ZVx4NzNceDJlXHg2M1x4NmZceDZkXHg3MFx4NjlceDZjXHg2NVx4MjhceDczXHgyY1x4MjBceDI3XHgzY1x4NzNceDc0XHg3Mlx4NjlceDZlXHg2N1x4M2VceDI3XHgyY1x4MjBceDI3XHg2NVx4NzhceDY1XHg2M1x4MjdceDI5XHgyY1x4MjBceDI3XHgyN1x4MmVceDZhXHg2Zlx4NjlceDZlXHgyY1x4MjBceDZkXHg2MVx4NzBceDJjXHgyMFx4NWJceDZjXHg2MVx4NmRceDYyXHg2NFx4NjFceDIwXHg3NFx4M2FceDIwXHg2M1x4NjhceDcyXHgyOFx4NjlceDZlXHg3NFx4MjhceDY2XHgyN1x4MzBceDYyXHg3Ylx4NzRceDdkXHgyN1x4MmNceDIwXHgzMlx4MjlceDI5XHgyY1x4MjBceDY0XHg2MVx4NzRceDYxXHg1ZFx4NWQnKQppWzBdKGlbMV0oaVsyXShpWzNdKCppWzRdKSkpKQ==',
-    "pyc": None,
-    "layers": b'e3NvdXJjZX0=',
-}
-BASE64: list = [
+ENCRIPTION = [
     "base64",
     "base16",
     "base32",
-    "base85"
+    "base85",
+    "bz2",
+    "zlib",
+    "marshal",
+    "eval",
+    "eval2",
+    "binary",
+    "pyc",
+    "multiple",
+    "layers",
+    
 ]
+
 
 class PyPrivate:
     
-    def __init__(self, path: str, model: str) -> None:
-        self.size_file = __import__('size').Size
-        print("# Start Encryption Algorithm ...")
-        self.path = path
-        self.model = model
-        self.syntax: str = None
-        if os.path.isdir(path):
-            for _path, _dirs, _files in os.walk(path):
-                for file in _files:
-                    if file.endswith('.py'):
-                        self.path = os.path.join(_path, file)
-                        if model == "pyc":
-                            self.pyc
-                            print('\x1b[0m# \x1b[0;32mpyc ✓')
-                        else:
-                            self.source = self.reader
-                            self._bytes = self.bytes
-                            self.encryption
-            print("\x1b[0m# \x1b[0;33mDone ✓")
-            return
+    def __init__(self, filename: str, model: str) -> None:
+            self.newtext = ''
+            self.path = filename
+            self.run_function(model)
+
+
+    def run_function(self, model: str) -> None:
+        assert model in ENCRIPTION
+        try:
+            if model.startswith("base"):
+                getattr(self, "base64")(model)
+            else:
+                getattr(self, model)()
+            print(f"\x1b[0;34m# \x1b[0;32mEncode \x1b[0;33m{model} \x1b[0;34m✓")
+        except FileExistsError as e:
+            print(f"\x1b[0;34m# \x1b[0;31mERROR \x1b[0;33m{model} \x1b[0;34m=> \x1b[0;31m{e.__class__.__name__}: \x1b[0;0m{str(e)}")
+
+    def _show_code(self):
+        if self.newtext:
+            newtext = self.newtext.decode()
         else:
-            if model == "pyc":
-                 self.pyc
-                 exit('\x1b[0m# \x1b[0;32mpyc ✓\n\x1b[0m# \x1b[0;33mDone ✓')
-            self.source = self.reader
-            self._bytes = self.bytes   
-        self.encryption
-        print("\x1b[0m# \x1b[0;33mDone ✓")
-        
-        
-    @property
-    def encryption(self) -> None:
-        content_file = self.syntax.format(source=self._bytes).encode()
-        with open(self.path, "wb") as f:
-            f.write(content_file)
-        print(f"\x1b[0m# \x1b[0;32m{self.model} ✓")
-        print(f"\x1b[0;33m{self.path}\x1b[0;31m:", self.size_file(self.path).size)
-        Console().print(Syntax(content_file.decode(), 'python'))
-        
-        
-    @property
-    def reader(self) -> bytes:
-        check_copyright = f"""
-        \rwith open(__file__, 'rb') as f:
-        \r    if not ({COPYRIGHT.encode()} in f.read()):
-        \r        __import__('builtins').exit(f\"\"\"Copyright not found!\ntry write:\n\x1b[0;32m{COPYRIGHT}\x1b[0m\nin {{__file__}}\"\"\")\n""".encode()
+            with open(self.path, "r") as f:
+                newtext = f.read()
+        Console().print(Syntax(newtext, "python"))
+
+    def show_code(self):
+        thread = multiprocessing.Process(target=self._show_code)
+        thread.start()
+        thread.join(4)
+        if thread.is_alive():
+            thread.kill()
+            print("\x1b[0;34m# \x1b[0;33mcan't show the code because the file is the big!")
+
+
+    def code_eval(self, text: str) -> str:
+        code = ''.join(map(lambda a: f'\{hex(ord(a))[1:]}',text))
+        return f"'{code}'"
+
+
+    def read(self) -> bytes:
+        check = f"""with open(__file__, 'rb') as check:exit(f\"\"\"Copyright not found!\ntry write:\n\x1b[0;32m{copyright.decode()}\x1b[0m\nin {{__file__}}\"\"\") if not ({copyright} in check.read()) else None\n""".encode()
         with open(self.path, "rb") as f:
-            return check_copyright + f.read()
-
-    @property
-    def bytes(self) -> bytes:
-        if self.model in BASE64:
-            self.syntax = DECODE_SYNTAX(b'aW1wb3J0IGJhc2U2NAppbXBvcnQgYnVpbHRpbnMKYnVpbHRpbnMuZXhlYyhidWlsdGlucy5jb21waWxlKGJhc2U2NC5ie251bX1kZWNvZGUoe3tzb3VyY2V9fSksICI8c3RyaW5nPiIsICJleGVjIikp').format(
-                num=self.model[-2:]
-            )
-            return self.base64
-        elif self.model in list(SYNTAX_ENCRYPTION.keys()):
-            self.syntax = DECODE_SYNTAX(SYNTAX_ENCRYPTION[self.model])
-            return eval(f"self.{self.model}")
-        else:
-            raise AttributeError(f"'{self.model}'")
-            
-    @property
-    def bz2(self) -> bytes:
-        return bz2.compress(self.source)
-
-    @property
-    def zlib(self) -> bytes:
-        return zlib.compress(self.source)
-        
-    @property
-    def base64(self) -> bytes:
-        return eval(f"base64.b{self.model[-2:]}encode")(self.source)
+            return check + f.read()
     
-    @property
-    def marshal(self) -> bytes:
-        return marshal.dumps(compile(self.source, "<string>", "exec"))
+    def write(self, newtext: bytes) -> None:
+        with open(self.path, "wb") as f:
+            self.newtext = (copyright + (newtext if type(newtext) == bytes else newtext.encode()))
+            f.write(self.newtext)
 
-    @property
+    def base64(self, model: str) -> None:
+        self.write(f"""import builtins\nimport base64\nbuiltins.exec(builtins.compile(base64.b{model[-2:]}decode({getattr(base64, 'b' + model[-2:] + 'encode')(self.read())}), "<string>", "exec"))""")
+
+    def bz2(self) -> None:
+        self.write(f"""import builtins\nx = lambda t, m: eval(f"__import__(\'{{m}}\'){{t}}press")({bz2.compress(self.read())})\nbuiltins.exec(builtins.compile(x(".decom", "2zb"[::-1]),"string", "exec"))""")
+    
+    def zlib(self) -> None:
+        self.write(f"""import builtins\nx = lambda t, m: eval(f"__import__(\'{{m}}\'){{t}}press")({zlib.compress(self.read())})\nbuiltins.exec(builtins.compile(x(".decom", "bilz"[::-1]),"string", "exec"))""")
+
+    def marshal(self) -> None:
+        self.write(f"""import builtins\nimport marshal as m\nbuiltins.exec(m.loads({marshal.dumps(compile(self.read(), '<string>', 'exec'))}))""")
+    
+    def eval(self) -> None:
+        encode = ''.join(map(lambda t: chr(eval(f'ord(t) {"-" if ord(t) + 100 > 1114111 else "+"} 100')), self.read().decode())).encode()
+        self.write(f"""import builtins\ndata = ({encode}, eval('\\x5b\\x62\\x75\\x69\\x6c\\x74\\x69\\x6e\\x73\\x2e\\x65\\x78\\x65\\x63\\x2c\\x20\\x6c\\x61\\x6d\\x62\\x64\\x61\\x20\\x63\\x3a\\x20\\x62\\x75\\x69\\x6c\\x74\\x69\\x6e\\x73\\x2e\\x63\\x6f\\x6d\\x70\\x69\\x6c\\x65\\x28\\x63\\x2c\\x20\\x27\\x3c\\x73\\x74\\x72\\x69\\x6e\\x67\\x3e\\x27\\x2c\\x20\\x27\\x65\\x78\\x65\\x63\\x27\\x29\\x2c\\x20\\x6c\\x61\\x6d\\x62\\x64\\x61\\x20\\x74\\x3a\\x20\\x63\\x68\\x72\\x28\\x28\\x6f\\x72\\x64\\x28\\x74\\x29\\x2d\\x31\\x30\\x30\\x29\\x20\\x20\\x69\\x66\\x20\\x6f\\x72\\x64\\x28\\x74\\x29\\x20\\x2b\\x20\\x31\\x30\\x30\\x20\\x3e\\x20\\x31\\x31\\x31\\x34\\x31\\x31\\x31\\x20\\x65\\x6c\\x73\\x65\\x20\\x28\\x6f\\x72\\x64\\x28\\x74\\x29\\x20\\x2d\\x20\\x31\\x30\\x30\\x29\\x29\\x2c\\x20\\x6d\\x61\\x70\\x2c\\x20\\x6c\\x61\\x6d\\x62\\x64\\x61\\x20\\x74\\x3a\\x20\\x27\\x27\\x2e\\x6a\\x6f\\x69\\x6e\\x28\\x74\\x29\\x5d'))\ndata[1][0](data[1][1](data[1][-1](data[1][-2](data[1][-3], data[0].decode()))))""")
+
+    def eval2(self) -> None:
+        code_marshal = marshal.dumps(compile(self.read(), "<string>", "exec"))
+        self.write(f"""eval({self.code_eval('builtins.exec')})(eval({self.code_eval('__import__')})(eval({self.code_eval('"marshal"')})).loads({code_marshal}))""")
+
+    def binary(self) -> None:
+        data = list(map(lambda t: int(bin(ord(t))[2:]), self.read().decode()))
+        self.write(f"""import builtins\ndata = ({data}, eval("\\x5b\\x62\\x75\\x69\\x6c\\x74\\x69\\x6e\\x73\\x2e\\x65\\x78\\x65\\x63\\x2c\\x20\\x6c\\x61\\x6d\\x62\\x64\\x61\\x20\\x63\\x3a\\x20\\x62\\x75\\x69\\x6c\\x74\\x69\\x6e\\x73\\x2e\\x63\\x6f\\x6d\\x70\\x69\\x6c\\x65\\x28\\x63\\x2c\\x20\\x27\\x3c\\x73\\x74\\x72\\x69\\x6e\\x67\\x3e\\x27\\x2c\\x20\\x27\\x65\\x78\\x65\\x63\\x27\\x29\\x2c\\x20\\x6c\\x61\\x6d\\x62\\x64\\x61\\x20\\x74\\x3a\\x20\\x63\\x68\\x72\\x28\\x69\\x6e\\x74\\x28\\x66\\x27\\x30\\x62\\x7b\\x74\\x7d\\x27\\x2c\\x20\\x32\\x29\\x29\\x2c\\x20\\x6d\\x61\\x70\\x2c\\x20\\x6c\\x61\\x6d\\x62\\x64\\x61\\x20\\x74\\x3a\\x20\\x27\\x27\\x2e\\x6a\\x6f\\x69\\x6e\\x28\\x74\\x29\\x5d"))\ndata[1][0](data[1][1](data[1][-1](data[1][-2](data[1][-3], data[0]))))""")
+        
     def pyc(self) -> None:
         try:
             py_compile.compile(self.path, f"{self.path}c", doraise=True)
         except py_compile.PyCompileError as e:
             raise SyntaxError(f"{e.exc_value}")
-
-    @property
-    def layers(self) -> str:
-        print(f'\x1b[0;34mlayers:')
-        big_time = ['binary', 'bz2', 'eval2']
-        for model in sorted(list(SYNTAX_ENCRYPTION.keys())[:-2] + BASE64):
-            if model in big_time:
-                print(f'  \x1b[0;31m{model} \x1b[0;34m# It takes a lot time!')
-                continue
-            self.model = model
-            _bytes = self.bytes
-            self.source = self.syntax.format(
-                source=_bytes
-            ).encode()
-            print(f'  \x1b[0;32m{model} ✓')
-        self.syntax = DECODE_SYNTAX(SYNTAX_ENCRYPTION["layers"])
-        self.model = "layers"
-        return self.source.decode()
-
-    @property
-    def eval(self) -> str:
-        _eval = lambda : ''.join(map(lambda t: chr(ord(t)**2), self.source.decode()))
-        try:
-            return _eval()
-        except ValueError:
-            self.model = "base64"
-            _bytes = self.bytes
-            self.source = self.syntax.format(
-                source=_bytes
-            ).encode()
-            self.model = "eval"
-            self.syntax = DECODE_SYNTAX(SYNTAX_ENCRYPTION["eval"])
-            return _eval()
-
     
-    @property
-    def eval2(self) -> str:
-        m = marshal.dumps(compile(self.source, 'string', 'exec'))
-        x = lambda t: ''.join(map(lambda a: f'\{hex(ord(a))[1:]}',t))
-        return x(f"builtins.eval(''.join(builtins.eval(\"{x('map')}\")(builtins.eval(\"{x('chr')}\"), [95, 95, 105, 109, 112, 111, builtins.eval(\"{x('114')}\"), 116, 95, 95, 40, builtins.eval(\"{x('39')}\"), 109, 97, 114, builtins.eval(\"{x('115')}\"), 104, 97, 108, 39, 41, 46, builtins.eval(\"{x('108')}\"), 111, 97, builtins.eval(\"{x('100')}\"), 115])))({m})")
+    def layers(self) -> None:
+        for model in ENCRIPTION:
+            if model in ["base32", "bz2", "eval", "binary", "pyc", "layers"]:
+                continue
+            PyPrivate(self.path, model)
+    
+    def multiple(self) -> None:
+        code = marshal.dumps(compile(self.read(), "<string>", "exec"))
+        d = len(code) // 50
+        d = d if d > 0 else 1
+        _range = range(0, len(code) + d, d)
+        encode = f"builtins.exec(m.loads(b''.join([{','.join(f'i{a}' for a in _range)}])))".encode()
+        first = 0
+        for index in _range:
+            encode = f"import builtins\nimport marshal as m\ni{index}={repr(code[first:index])}\nbuiltins.exec(m.loads({marshal.dumps(compile(encode, '<string>', 'exec'))}))".encode()
+            first = index
+        self.write(encode)
 
 
-    @property
-    def binary(self) -> str:
-        return str(list(map(lambda t: int(bin(ord(t))[2:]), self.source.decode())))
+def pyprivate_with_progress(path: str, model: str) -> None:
+    size = __import__("size").Size
+    with Progress(
+        TextColumn("[progress.desciption]{task.description}"),
+        SpinnerColumn(spinner_name="point"),
+        transient=True,
+    ) as progress:
+        
+        progress.add_task(f"[cyan]{model}")
+        py = PyPrivate(path, model)
+        print("\x1b[0;34m# \x1b[0;32mSize  ",size(path, anim=False).size)
+        
+    py.show_code()
+    print("\x1b[0;34m# Done ✓")
+        
+
+def massage_help():
+    print('\x1b[0;32mencryption:')
+    for x in range(len(ENCRIPTION)):
+        point = str(round(x /  (len(ENCRIPTION)-1)*100))
+        print(' '*(4-len(point)) + '   \x1b[0;34m' + point + "%\x1b[0;33m", ENCRIPTION[x])
+    print('\x1b[0;32mexamples:\x1b[0;33m\n    $ pyprivate file.py marshal\n    $ pyprivate path/mydir layers\n    $ pyprivate file.py bz2\n    $ pyprivate file.py lambda'.replace("pyprivate", "\x1b[0;32mpyprivate\x1b[0;33m"))
+    exit()
 
 
-
-            
-            
 if __name__ == "__main__":
     argv = sys.argv[1:]
-    error = f"example:\n    $ pyprivate path/mytool marshal\n    $ pyprivate path/myfile eval2\n\nencryption:\n    {f'{chr(10)}    '.join(set(list(SYNTAX_ENCRYPTION.keys()) + BASE64))}"
+    print("# Start Encryption Algorithm ...")  
     if len(argv) >= 2:
         path, model = argv[:2]
     elif len(argv) < 2:
-        exit(error)
+        massage_help()
     else:
-        exit(error)
+        massage_help()
     try:
-        PyPrivate(path, model)
+        pyprivate_with_progress(path, model)
     except ModuleNotFoundError:
-        exit("No HackerMode !!!")
+        exit("# No HackerMode !!!")
     except Exception as e:
-        exit(f'{e.__class__.__name__}: {str(e)}\n'+error)
+        print(f'{e.__class__.__name__}: {str(e)}')
+        massage_help()
+        
+        
+
